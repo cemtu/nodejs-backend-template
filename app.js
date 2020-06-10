5874a21ebd2edf7ed0b8fb87a939cf68e5992e1f
BayCem <cem_tuncelli@protonmail.ch>
2020-06-11 01:30:29 +0300

    Swagger added.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const apiRoutes = require('./api/index');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
 app.use('/api', apiRoutes);
-
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 module.exports = app;
