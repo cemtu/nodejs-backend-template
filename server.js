62b99cf94dd35eb54a8c43ba2f6a4e8f78216a85
BayCem <cem_tuncelli@protonmail.ch>
2020-06-25 18:02:49 +0300

    server.js configured.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
-process.env.NODE_ENV = 'development';
-// process.env.NODE_ENV = 'staging';
+// process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'staging';
 // process.env.NODE_ENV = 'production';
 
 // eslint-disable-next-line no-unused-vars
