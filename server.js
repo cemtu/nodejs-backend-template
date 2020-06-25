6df01c05828e84dc47d14fcb9a8e2da973b5abea
BayCem <cem_tuncelli@protonmail.ch>
2020-06-25 18:09:38 +0300

    server.js updated.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const app = require('./app');
 
-process.env.NODE_ENV = 'development';
+// process.env.NODE_ENV = 'development';
 // process.env.NODE_ENV = 'staging';
-// process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = 'production';
 
 // eslint-disable-next-line no-unused-vars
 const config = require('./config/config.js');
